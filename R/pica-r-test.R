88de8bc59d77ed32b698050b244afdc6fdb38147
vallooy <65543379+vallooy@users.noreply.github.com>
2020-11-12 15:05:01 +0100

    Update pica-r-test.R

--- R/pica-r-test.R
+++ R/pica-r-test.R
@@ -1,13 +1,13 @@
 check <- function(x) {
   x <- as.numeric(x)
   if (x > 0) {
-    result <- "Guilhem, ceci est positif!"
+    result <- "Guilhem, selon R, ceci est positif!"
   }
   else if (x < 0) {
-    result <- "Guilhem, ceci est négatif!"
+    result <- "Guilhem, selon R, ceci est négatif!"
   }
   else {
-    result <- "Guilhem, ça c'est nul!"
+    result <- "Guilhem, selon R, ça c'est nul!"
   }
   list(
     message = paste(result)
