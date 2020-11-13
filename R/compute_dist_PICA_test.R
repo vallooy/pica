c4e6f1883fc2c555725c1726a430f5368db98874
vallooy <65543379+vallooy@users.noreply.github.com>
2020-11-13 11:47:58 +0100

    Update compute_dist_PICA_test.R

--- R/compute_dist_PICA_test.R
+++ R/compute_dist_PICA_test.R
@@ -7,10 +7,10 @@ Calcul_fourmis_distance <- function() {
   parcelle <- read.csv("https://cloud.opencpu.org/ocpu/apps/vallooy/pica/www/Parcelle_simulated_Estagnol_2017.csv", sep = ",", dec = ".",header = T)  
   #La liste des points de la parcelle
   
-  liste_sites <- read.csv("https://cloud.opencpu.org/ocpu/apps/vallooy/pica/www/data_to_python_matrix_estagnol.csv", sep = ";", dec = ",",header = T)  
+  liste_sites <- read.csv("https://cloud.opencpu.org/ocpu/apps/vallooy/pica/www/data_to_python_matrix_estagnol.csv", sep = ",", dec = ".",header = T)  
   # La liste des individus associés aux ligne de la matrice de distance (un individu apparait deux fois, une fois pour chaque interang
     
-  dist_mat <- read.csv("https://cloud.opencpu.org/ocpu/apps/vallooy/pica/www/test_dist_estagnol2.csv", sep = ";", dec = ",",header = T)  
+  dist_mat <- read.csv("https://cloud.opencpu.org/ocpu/apps/vallooy/pica/www/test_dist_estagnol2.csv", sep = ";", dec = ",")  
   # La matrice de distance, assez long à charger
   
   #entree_file <- paste0(dossier,"Point_entree_",parcelle_ID,".txt")
